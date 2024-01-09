a22212e2d6b021e0f7ff2b4d4c06cc1d205f3db2
dewacandra4 <45729819+dewacandra4@users.noreply.github.com>
2024-01-09 12:34:50 +0800

    Refactored and improved our app service to support a new AI assistant functionality by adding the assistant ID configuration.

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,10 +5,12 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  private readonly openai: OpenAI;
-  private readonly telegramBot: TelegramBot; // Adjust the type based on your actual usage
+  private readonly openai: OpenAI; // use your openai api key from https://platform.openai.com/api-keys
+  private readonly telegramBot: TelegramBot; // use your telegram bot token, that you create from @BotFather
+  private readonly assistantId: string; // use assistant id that you create from your open ai account
 
   constructor(private readonly configService: ConfigService) {
+    // initialize openai, telegram bot and assistant id
     this.openai = new OpenAI({
       apiKey: this.configService.get('OPENAI_API_KEY'),
     });
@@ -20,21 +22,29 @@ export class AppService {
       },
     );
 
+    this.assistantId = this.configService.get('ASSISTANT_ID');
+
     this.setupTelegramBot();
   }
 
+  /**
+   * Sets up the Telegram bot and handles incoming messages.
+   *
+   * @param {type} msg - the incoming message from Telegram
+   * @return {type} - the response message sent by the bot
+   */
   setupTelegramBot() {
     this.telegramBot.onText(/.*/, async (msg) => {
       const userQuestion = msg.text;
-      const assistant = await this.createAssistant();
       const thread = await this.createThread();
+      console.log('user question', userQuestion);
 
-      await this.createMessage(thread.id, 'user', userQuestion);
-      const run = await this.createRun(thread.id, assistant.id);
+      await this.createMessage(thread.id, userQuestion);
+      const run = await this.createRun(thread.id, this.assistantId);
       let runStatus = await this.retrieveRunStatus(thread.id, run.id);
 
       while (runStatus.status !== 'completed') {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         runStatus = await this.retrieveRunStatus(thread.id, run.id);
       }
 
@@ -45,7 +55,7 @@ export class AppService {
             message.run_id === run.id && message.role === 'assistant',
         )
         .pop();
-
+      console.log('last message for run', lastMessageForRun.content);
       if (lastMessageForRun) {
         this.telegramBot.sendMessage(
           msg.chat.id,
@@ -60,38 +70,39 @@ export class AppService {
     });
   }
 
-  async createAssistant(): Promise<any> {
-    return this.openai.beta.assistants.create({
-      name: 'Holo',
-      instructions:
-        'You are a customer support chatbot. Use your knowledge base to best respond to customer queries.',
-      tools: [{ type: 'retrieval' }],
-      model: 'gpt-4-1106-preview',
-      file_ids: ['file-OlktVqMJkuD1jsMfqC9wdt52'],
-    });
-  }
-
+  /**
+   * Creates a new thread.
+   *
+   */
   async createThread(): Promise<any> {
     return this.openai.beta.threads.create();
   }
 
-  async createMessage(
-    threadId: string,
-    role: string,
-    content: string,
-  ): Promise<any> {
+  async createMessage(threadId: string, content: string): Promise<any> {
     return this.openai.beta.threads.messages.create(threadId, {
       role: 'user',
       content,
     });
   }
 
+  /**
+   * Creates a run for a given thread and assistant.
+   *
+   * @param {string} threadId - The ID of the thread.
+   * @param {string} assistantId - The ID of the assistant.
+   */
   async createRun(threadId: string, assistantId: string): Promise<any> {
     return this.openai.beta.threads.runs.create(threadId, {
       assistant_id: assistantId,
     });
   }
 
+  /**
+   * Retrieves the status of a run given the thread ID and run ID.
+   *
+   * @param {string} threadId - The ID of the thread.
+   * @param {string} runId - The ID of the run.
+   */
   async retrieveRunStatus(threadId: string, runId: string): Promise<any> {
     return this.openai.beta.threads.runs.retrieve(threadId, runId);
   }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,5 +2,6 @@ export default () => ({
   APP: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    ASSISTANT_ID: process.env.ASSISTANT_ID,
   },
 });
